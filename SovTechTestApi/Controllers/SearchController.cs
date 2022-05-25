79b10666b7d5d64e7f1c49e90a53e4728d61c3c2
leon.muchoki@thejitu.com <leon.muchoki@thejitu.com>
2022-05-25 10:28:17 +0300

    add search endpoint

---
+++ SovTechTestApi/Controllers/SearchController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SovTechTestApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SovTechTestApi.Controllers
+{
+    [Route("api/search")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        public const string CHUCK_SEARCH_URL = "https://api.chucknorris.io/jokes/search?query=";
+        public const string SWAPI_SEARCH_URL = "https://swapi.dev/api/people/?search=";
+
+        [HttpGet]
+        public async Task<Dictionary<string, dynamic>> Get([FromQuery] SearchQuery query)
+        {
+            var queryResult = new Dictionary<string, dynamic>();
+            using (var httpClient = new HttpClient())
+            {
+                if(query.chuck_query != null)
+                {
+                    using (var response = await httpClient.GetAsync(CHUCK_SEARCH_URL + query.chuck_query.Trim()))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        var res = JsonConvert.DeserializeObject<SearchChuckResult>(apiResponse);
+                        queryResult.Add("chuck", res);
+                    }
+                }
+
+                if (query.swapi_query != null)
+                {
+                    using (var response = await httpClient.GetAsync(SWAPI_SEARCH_URL + query.swapi_query.Trim()))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        var res = JsonConvert.DeserializeObject<SwapiPeopleResult>(apiResponse);
+                        queryResult.Add("swapi", res);
+                    }
+                }
+
+            }
+
+            return queryResult;
+        }
+
+    }
+}
---
+++ SovTechTestApi/Models/SearchQuery.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SovTechTestApi.Models
+{
+    public class SearchQuery
+    {
+        public string? chuck_query { get; set; }
+        public string? swapi_query { get; set; }
+    }
+    public class SearchChuckResult
+    {
+        public int? total { get; set; }
+        public List<Chuck> result { get; set; }
+    }
+
+    public class Chuck
+    {
+        public string[] categories { get; set; }
+        public string created_at { get; set; }
+        public string icon_url { get; set; } 
+        public string id { get; set; }
+        public string updated_at { get; set; }
+        public string url { get; set; }
+        public string value { get; set; }
+    }
+
+}
