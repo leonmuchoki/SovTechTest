bd32d29763775b373a0aae0d309ee44d6456e0fe
leon.muchoki@thejitu.com <leon.muchoki@thejitu.com>
2022-05-25 14:57:41 +0300

    add endpoint for category detail

--- SovTechTestApi/Controllers/ChuckController.cs
+++ SovTechTestApi/Controllers/ChuckController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using SovTechTestApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,7 @@ namespace SovTechTestApi.Controllers
     public class ChuckController : ControllerBase
     {
         public const string CHUCK_CATEGORIES_URL = "https://api.chucknorris.io/jokes/categories";
+        public const string CHUCK_CATEGORY_URL = "https://api.chucknorris.io/jokes/random?category=";
 
         [HttpGet, Route("/categories")]
         public async Task<IEnumerable<string>> GetChuckCategoriesAsync()
@@ -29,5 +31,22 @@ namespace SovTechTestApi.Controllers
 
             return chuckCategories;
         }
+
+        [HttpGet("{category}")]
+        public async Task<ChuckCategory> GetChuckCategoryAsync(string category)
+        {
+            var chuckCategory = new ChuckCategory();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(CHUCK_CATEGORY_URL + category.Trim()))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    chuckCategory = JsonConvert.DeserializeObject<ChuckCategory>(apiResponse);
+                }
+            }
+
+            return chuckCategory;
+        }
+
     }
 }
