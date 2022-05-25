e043c41258f5a64f83eb4b66e8e500e15847097c
leon.muchoki@thejitu.com <leon.muchoki@thejitu.com>
2022-05-25 09:51:43 +0300

    add swapi people endpoint

---
+++ SovTechTestApi/Controllers/SwapiController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SovTechTestApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SovTechTestApi.Controllers
+{
+    [Route("api/swapi")]
+    [ApiController]
+    public class SwapiController : ControllerBase
+    {
+        public const string SWAPI_PEOPLE_URL = "https://swapi.dev/api/people";
+
+        [HttpGet, Route("/people")]
+        public async Task<IEnumerable<SwapiPeople>> GetSwapiPeopleAsync()
+        {
+            List<SwapiPeople> swapiPeople = new List<SwapiPeople>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(SWAPI_PEOPLE_URL))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    var res = JsonConvert.DeserializeObject<SwapiPeopleResult>(apiResponse);
+                    swapiPeople = res.results;
+                }
+            }
+
+            return swapiPeople;
+        }
+    }
+}
---
+++ SovTechTestApi/Models/SwapiPeople.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SovTechTestApi.Models
+{
+    public class SwapiPeopleResult
+    {
+        public string count { get; set; }
+        public string? next { get; set; }
+        public string? previous { get; set; }
+        public List<SwapiPeople> results { get; set; }
+    }
+    public class SwapiPeople
+    {
+        public string name { get; set; }
+        public string height { get; set; }
+        public string mass { get; set; }
+        public string hair_color { get; set; }
+        public string skin_color { get; set; }
+        public string eye_color { get; set; }
+        public string birth_year { get; set; }
+        public string gender { get; set; }
+        public String[] films { get; set; }
+        public string[] species { get; set; }
+        public string[] vehicles { get; set; }
+        public string[] starships { get; set; }
+        public string created { get; set; }
+        public string edited { get; set; }
+        public string url { get; set; }
+    }
+}
